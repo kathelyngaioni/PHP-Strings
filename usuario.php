ae91ee1797ba4a3a3662c74609fb4d32434d97b9
Kathelyn Gaioni <112026988+kathelyngaioni@users.noreply.github.com>
2023-04-05 13:41:37 +0000

    explode em PHP

--- usuario.php
+++ usuario.php
@@ -2,15 +2,21 @@
 //strpos : posição
 //Criar o nome do usuario e dominio a partir do email:
 
+$informacoes = 'Kathelyn Gaioni , 24 , gaioni.kathelyn@outlook.com';
 $email = 'gaioni.kathelyn@outlook.com';
 $posicaoArroba = strpos($email,'@');
+
                           //inicio   //fim
 $usuario = substr($email, 0, $posicaoArroba); //substring
 $dominio = substr($email, $posicaoArroba + 1);
 echo $usuario . PHP_EOL ;
 echo $dominio . PHP_EOL;
 
-
-
+//explode retorna um array
+                                 //separador
+List($nome, $idade, $email) = explode(',',$informacoes);
+echo 'Nome: ' . $nome . PHP_EOL;
+echo 'Idade: ' . $idade . PHP_EOL;
+echo 'Email: ' . $email . PHP_EOL;
 
 ?>
